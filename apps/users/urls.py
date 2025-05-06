cb5cb9eed2920e70159b951195a91218f9be324d
Sotnikov Oleksandr <ssotnikovvv55@gmail.com>
2025-05-06 19:28:39 +0300

    Migrate from drf-yasg to drf-spectacular while ensuring proper parameter configuration

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenBlacklistView
+
 from apps.users.views import UserRegistrationView
+from apps.users.auth_views import CustomTokenObtainPairView, CustomTokenRefreshView
 
-
-urlpatterns = [
-    path("register/", UserRegistrationView.as_view(), name="user-register"),
-    path("token/", TokenObtainPairView.as_view(), name="token-obtain"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
-]
 
 app_name = "users"
+
+urlpatterns = [
+    path("register/", UserRegistrationView.as_view(), name="register"),
+    path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),
+    path("token/blacklist/", TokenBlacklistView.as_view(), name="token_blacklist"),
+]
